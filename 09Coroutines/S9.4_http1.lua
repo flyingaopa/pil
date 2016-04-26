4a5ede01ff654bea49dd942edf8d0764984600e3
cloud <local>
2016-04-26 03:41:04 -0400

    rename

--- 09Coroutines/S9.4_http1.lua
+++ 09Coroutines/S9.4_http1.lua
@@ -3,19 +3,16 @@ local host = "www.w3.org"
 local file = "/TR/REC-html32.html"
 
 local sk = so.socket(so.AF_INET, so.SOCK_STREAM, 0)
-sk:setdebug(true)
-
 sk:connect(host, 80)
-sk:setblocking(0)
 
 sk:send("GET " .. file  .. " HTTP/1.0\r\n\r\n")
 while true do
     local msg, err = sk:recv(1024)
     if msg then
         if #msg == 0 then break end
-        --io.write(msg)
+        io.write(msg)
     else
---        print(err)
+        print(err)
     end
 end
 
